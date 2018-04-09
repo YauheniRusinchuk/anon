4adfbd2142a07b7a1c9e0f5b66c8a7aba4e02813
YauheniRusinchuk <ruevgal@gmail.com>
2018-04-09 17:42:54 +0300

    add header btn

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,14 @@ import React, {Fragment} from 'react';
 import './style/header.css';
 
 import Addpostbtn from './addpostbtn';
+import Homepage from './homepage';
 
 
 export default () => {
   return(
     <Fragment>
       <div className="header">
+        <Homepage />
         <Addpostbtn />
       </div>
     </Fragment>
