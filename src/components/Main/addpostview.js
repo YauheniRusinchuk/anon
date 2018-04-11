b005d911171f42eebfaae0e07d8e810ea27bc779
YauheniRusinchuk <ruevgal@gmail.com>
2018-04-11 16:36:39 +0300

    add form post

--- src/components/Main/addpostview.js
+++ src/components/Main/addpostview.js
@@ -3,8 +3,16 @@ import React from 'react';
 
 export default () => {
   return(
-    <div className="addpostview">
-
-    </div>
+    <div className='addpostview'>
+      <div className="container">
+		     <form>
+			     <input className="one" type="text" placeholder="Title...." />
+		 	     <textarea placeholder="Content..." className="two"></textarea>
+			     <div className="button_add_post">
+				      <input  className="three" type="submit" value="Add" / >
+			     </div>
+		    </form>
+	   </div>
+  </div>
   );
 }
